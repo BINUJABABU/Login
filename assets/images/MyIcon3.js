546de57aab5a67e4d0a12558886655da9ddc9268
user <binujababu>
2024-08-23 10:00:18 +0530

    first commit

---
+++ assets/images/MyIcon3.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Svg, { Path,Defs ,Stop, LinearGradient} from 'react-native-svg';
+
+const MyIcon3 = (props) => (
+    <Svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<Path d="M5 12H19M19 12L12 5M19 12L12 19" stroke="url(#paint0_linear_6446_8758)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<Defs>
+<LinearGradient id="paint0_linear_6446_8758" x1="5" y1="5" x2="29.9283" y2="26.9484" gradientUnits="userSpaceOnUse">
+<Stop stop-color="#FFE500"/>
+<Stop offset="0.0998634" stop-color="#FF9452"/>
+<Stop offset="0.20103" stop-color="#FF6F7E"/>
+<Stop offset="0.291745" stop-color="#F63FB8"/>
+<Stop offset="0.427243" stop-color="#C037EB"/>
+<Stop offset="0.541668" stop-color="#8A46FF"/>
+<Stop offset="0.654058" stop-color="#4558D8"/>
+<Stop offset="0.760167" stop-color="#4829CC"/>
+<Stop offset="1" stop-color="#2958A6"/>
+</LinearGradient>
+</Defs>
+</Svg>
+
+);
+
+export default MyIcon3;
