518a1f526739a3ddff43897fe7b30007b9820328
user <binujababu>
2024-08-26 14:14:12 +0530

    commit

--- App.tsx
+++ App.tsx
@@ -10,18 +10,19 @@ import MyIcon1 from './assets/images/MyIcon1'
 const Login=()=>{
    return(
     <SafeAreaView style={styles.safe}>
-      <StatusBar translucent={true} hidden={true} />
+     
+      <StatusBar translucent backgroundColor="transparent" />
       <ScrollView automaticallyAdjustKeyboardInsets={true} style={styles.scrollView}>
       <View style={styles.logoContainer}>
          <Image source={require('./assets/images/Vector.png')} style={styles.image1}></Image>
          <Image source={require('./assets/images/image2.png')}  style={styles.logo}></Image> 
             <View style={styles.action}>
                 <MyIcon style={styles.logo1}></MyIcon>
-                <TextInput  style={styles.textInput} placeholder="User name"></TextInput>
+                <TextInput  style={styles.textInput} placeholder="User name" placeholderTextColor={'#7B7B7B'}></TextInput>
             </View>
             <View style={styles.action}>
                 <MyIcon1 style={styles.logo1}></MyIcon1>
-                <TextInput  style={styles.textInput} placeholder="Password"></TextInput>
+                <TextInput  style={styles.textInput} placeholder="Password" placeholderTextColor={'#7B7B7B'}></TextInput>
             </View>
             <View style={styles.textview}>
                <Text style={styles.text}>
@@ -30,8 +31,8 @@ const Login=()=>{
             </View>
             <View style={{flex:1, width:'38%'}}>
                  <LinearGradient
-                  colors={['#FFE500', '#FF9452', '#FF6F7E', '#F63FB8', '#C037EB', '#8A46FF','#4558D8','#4829CC','#2958A6']}
-                     start={{ x: 0.0, y: 1.0 }} end={{ x: 1.0, y: 1.0 }}
+                  colors={['#FFE500','#FF9452', '#FF6F7E', '#F63FB8', '#C037EB', '#8A46FF']}
+                     start={{ x: 0.1, y: 1.0 }} end={{ x: 1.0, y: 0.0 }}
                      style={styles.grediant}>
                      <TouchableOpacity style={styles.buttonContainer}>
                       <Text style={styles.buttonText}>
@@ -40,27 +41,24 @@ const Login=()=>{
                         <Icon name="arrow-right" size={15} color="black" style={{color:'#c97caabf',alignSelf:'center',margin:10}}></Icon> 
                      </TouchableOpacity>
                   </LinearGradient>
-                  <View style={{flexDirection:'row',width:'100%',marginTop:30,flex:1}}>
+                  <View style={{flexDirection:'row',width:'100%',marginTop:35,flex:1}}>
                   <Text style={{fontWeight:'bold',fontSize:14}}>Don't have an account?</Text>
                   <Text style={{paddingLeft:5, color:'#8A46FF',textDecorationLine: 'underline'}}>Create</Text>
                   </View> 
                   <View style={{alignItems:'center'}}>
                   <Text>Continue With</Text>
                   </View>
+                
                  </View>
-                 
-                 <View style={{marginRight:80, flexDirection:'row',flex:1}}>
+                  <View style={{flex:1,position:'relative',marginRight:395,marginTop:152}}>
+                  <View style={{position:'absolute',left:0,bottom:0,flexDirection:'row'}} >
                   <Image source={require('./assets/images/image3.png')}  ></Image>
-                 
-                 <View style={{flexDirection:'row',marginTop:80,flex:1}}>
-                 <Icon name="facebook" size={32} color="#4267B2" style={styles.smallIcon}></Icon>
-                 
-                 <Image source={require('./assets/images/search.png')}  style={styles.search}></Image> 
-                 <Icon name="phone-alt" size={25} color="black" style={styles.smalllIcon}></Icon>
-                </View>
-                 
-                 </View>
-        </View>
+                  <View style={{flex:1,marginTop:100,marginLeft:50}}>
+                  <Image source={require('./assets/images/search.png')}  style={styles.search}></Image>
+                  </View>
+                  </View>
+                  </View>
+          </View>
       </ScrollView> 
   </SafeAreaView>
    )
@@ -82,22 +80,24 @@ const styles = StyleSheet.create({
   logo: {
     height: 200,
     width: 200,
-    marginTop: 5,
+    // marginTop: 1,
     resizeMode:'contain'
 },
 action: {
     flexDirection: 'row',
-    marginTop: 15,
+    alignItems:'center',
+    marginTop: 17,
     paddingHorizontal: 11,
-    borderWidth: 2,
+    paddingTop:6,
+    borderWidth: 1,
     borderColor:'white',
     borderRadius: 50,
     backgroundColor:'white',
     width:'90%',
-    height:50,
+    height:63,
     shadowColor: 'black',
-    shadowOpacity: 5,
-    shadowRadius: 50,
+    shadowOpacity: 10,
+    shadowRadius: 80,
     shadowOffset: {
       height: 0,
       width: 0,
@@ -110,28 +110,28 @@ smallIcon: {
 },
 textInput: {
     flex: 1,
-    fontSize:20,
-    color: '#05375a',
+    fontSize:18,
     marginLeft:8,
+   
 },
 text:{
     color:'#8A46FF',
     fontWeight:'400'
 },
 textview:{
-    justifyContent:'flex-end',
-    alignItems:'flex-end',
     marginTop:10,
-    marginLeft:200
+    marginLeft:200,
+    flex:1
 },
 grediant: {
     height: 44,
     width: "100%",
     justifyContent: 'center',
     alignSelf: 'center',
-    marginTop:30,
+    marginTop:20,
     padding:1,
-    borderRadius:50
+    borderRadius:40,
+    
 },
 buttonContainer: {
     flex: 1,
@@ -139,9 +139,10 @@ buttonContainer: {
     justifyContent: 'center',
     backgroundColor: '#ffffff',
     width: '100%',
-    margin: 1,
-    borderRadius: 50,
-    flexDirection:'row'
+    margin: 1.5,
+    borderRadius: 40,
+    flexDirection:'row',
+    
   },
 buttonText: {
     textAlign: 'center',
@@ -149,27 +150,14 @@ buttonText: {
     alignSelf: 'center',
     fontSize:20
 },
-lineStyle:{
-    borderWidth: 0.5,
-    borderColor:'black',
-    margin:10,
-},
 search:{
   resizeMode:'contain',
   height:37,
   width:29,
-  marginTop:7,
-  marginLeft:13,
-},
-smalllIcon: {
-  marginRight: 20,
-  padding:6,
-  marginLeft:18,
-  marginTop:5
 },
 logo1:{
   marginRight: 8,
- },
+},
 scrollView: {
   flex:1,
 }, 
